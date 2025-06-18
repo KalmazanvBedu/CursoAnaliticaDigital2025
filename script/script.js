6b8be8d8094fe1bdba3108c631177c70bfbc13a3
Keny 1 <keny.almazan@mrm.com>
2025-06-18 12:48:21 -0600

    2do commit - Arreglos y objetos

--- script/script.js
+++ script/script.js
@@ -4,3 +4,42 @@ const h2_uno = document.getElementById("h2-uno");
 //     alert("Clic en el H2");
 // });
 
+
+const cerveza = {
+
+    'nombre': 'XX',
+    'tipo': 'lager',
+    'envase': [
+        {
+            'nombre': 'caguama',
+            'capacidad': '1200ml', 
+            'color': 'ambar',
+            'material': 'vidrio'
+        },
+        {
+            'nombre': 'ampolleta',
+            'capacidad': '250ml', 
+            'color': 'ambar',
+            'material': 'vidrio'
+        },
+        {
+            'nombre': 'lata',
+            'capacidad': '335ml', 
+            'color': 'plata',
+            'material': 'aluminio'
+        },
+        {
+            'nombre': 'latón',
+            'capacidad': '473ml', 
+            'color': 'plata',
+            'material': 'aluminio'
+        }
+    ],
+    'subTipo': [
+        'clara',
+        'obscura',
+        'ambar'
+    ]
+
+}
+
